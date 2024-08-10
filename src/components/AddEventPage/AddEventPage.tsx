02ebe7a7f4fc5bfb79085e22ddc14c7a02d3b618
Facundo <facundoguisande11@hotmail.com>
2024-08-10 14:38:20 -0300

    Cambios en Agenda y cambios en Eventos. Acordar realizar cambio en el backend para que no surjan errores con la base

--- src/components/AddEventPage/AddEventPage.tsx
+++ src/components/AddEventPage/AddEventPage.tsx
@@ -1,27 +1,25 @@
-import React, { useState } from 'react';
-import { Form, Input, DatePicker, Button } from 'antd';
-import styled from 'styled-components';
-import { Select } from 'antd';
+import React, { useState } from "react";
+import { Form, Input, DatePicker, Button } from "antd";
+import styled from "styled-components";
+import { Select } from "antd";
 
 const { Option } = Select;
-
-
 
 const AddEventPageWrapper = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
   height: 98vh;
-  background-image: url('https://arteflame.com/cdn/shop/articles/The_10_Best_Pizza_Toppings_and_Why_They_Reign_Supreme.webp?v=1717620006&width=1600');
+  background-image: url("https://arteflame.com/cdn/shop/articles/The_10_Best_Pizza_Toppings_and_Why_They_Reign_Supreme.webp?v=1717620006&width=1600");
   background-size: cover;
   background-position: center;
   background-repeat: no-repeat;
   background-color: rgba(0, 0, 0, 0.5); /* Fondo semi-transparente */
 `;
 
-const AddEventContainer = styled.div` 
+const AddEventContainer = styled.div`
   background-color: #fff;
-  padding: 25px;
+  padding: 22px;
   border-radius: 8px;
   box-shadow: 0 2px 8px rgba(0, 0, 0, 0.15);
   width: 100%;
@@ -30,14 +28,15 @@ const AddEventContainer = styled.div`
 
 const AddEventPage: React.FC = () => {
   const [form] = Form.useForm();
-  const [clientName, setClientName] = useState('');
-  const [clientLastName, setClientLastName] = useState('');
-  const [clientId, setClientId] = useState('');
-  const [serviceType, setServiceType] = useState('');
+  const [clientName, setClientName] = useState("");
+  const [clientLastName, setClientLastName] = useState("");
+  const [clientId, setClientId] = useState("");
+  const [serviceType, setServiceType] = useState("");
   const [numPeople, setNumPeople] = useState(0);
   const [eventDate, setEventDate] = useState<Date | null>(null);
-  const [eventTime, setEventTime] = useState('');
-  const [eventLocation, setEventLocation] = useState('');
+  const [eventTime, setEventTime] = useState("");
+  const [eventLocation, setEventLocation] = useState("");
+  const [presupuesto, setpresupuesto] = useState(0);
 
   const handleSubmit = () => {
     // Aquí puedes implementar la lógica de envío del formulario
@@ -50,136 +49,180 @@ const AddEventPage: React.FC = () => {
       eventDate,
       eventTime,
       eventLocation,
+      presupuesto,
     });
   };
 
-
-
-  
-    const handleServiceTypeChange = (value: any) => {
-      setServiceType(value);
-    };
-
-
- 
+  const handleServiceTypeChange = (value: any) => {
+    setServiceType(value);
+  };
 
   return (
     <AddEventPageWrapper>
-    <AddEventContainer>
-      <h1>Agregar Evento</h1>
-      <Form form={form} layout="vertical" onFinish={handleSubmit} style={{ marginTop: '16px' }}>
-        <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'space-between' }}>
-          <Form.Item
-            label="Nombre del Cliente"
-            name="clientName"
-            rules={[{ required: true, message: 'Este campo es obligatorio' }]}
-            style={{ width: '48%' }}
+      <AddEventContainer>
+        <h1 style={{ textAlign: "center" }}>Agregar Evento</h1>
+        <Form
+          form={form}
+          layout="vertical"
+          onFinish={handleSubmit}
+          style={{ marginTop: "16px" }}
+        >
+          <div
+            style={{
+              display: "flex",
+              flexWrap: "wrap",
+              justifyContent: "space-between",
+            }}
           >
-            <Input
-              value={clientName}
-              onChange={(e) => setClientName(e.target.value)}
-            />
-          </Form.Item>
-          <Form.Item
-            label="Apellido del Cliente"
-            name="clientLastName"
-            rules={[{ required: true, message: 'Este campo es obligatorio' }]}
-            style={{ width: '48%' }}
-          >
-            <Input
-              value={clientLastName}
-              onChange={(e) => setClientLastName(e.target.value)}
-            />
-          </Form.Item>
-        </div>
-        <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'space-between' }}>
-          <Form.Item
-            label="ID del Cliente"
-            name="clientId"
-            rules={[{ required: true, message: 'Este campo es obligatorio' }]}
-            style={{ width: '48%' }}
-          >
-            <Input
-              value={clientId}
-              onChange={(e) => setClientId(e.target.value)}
-            />
-          </Form.Item>
-          <Form.Item
-            label="Tipo de Servicio"
-            name="serviceType"
-            rules={[{ required: true, message: 'Este campo es obligatorio' }]}
-            style={{ width: '48%' }}
-          >
-            <Select
-              value={serviceType}
-              onChange={handleServiceTypeChange}
-              style={{ width: '100%' }}
+            <Form.Item
+              label="Nombre del Cliente"
+              name="clientName"
+              rules={[{ required: true, message: "Este campo es obligatorio" }]}
+              style={{ width: "48%" }}
             >
-            <Option value="option1">Opción 1</Option>
-            <Option value="option2">Opción 2</Option>
-            <Option value="option3">Opción 3</Option>
-            <Option value="option4">Opción 4</Option>
-          </Select>
-          </Form.Item>
-        </div>
-        <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'space-between' }}>
-          <Form.Item
-            label="Número de Personas"
-            name="numPeople"
-            rules={[{ required: true, message: 'Este campo es obligatorio' }]}
-            style={{ width: '48%' }}
+              <Input
+                value={clientName}
+                onChange={(e) => setClientName(e.target.value)}
+              />
+            </Form.Item>
+            <Form.Item
+              label="Apellido del Cliente"
+              name="clientLastName"
+              rules={[{ required: true, message: "Este campo es obligatorio" }]}
+              style={{ width: "48%" }}
+            >
+              <Input
+                value={clientLastName}
+                onChange={(e) => setClientLastName(e.target.value)}
+              />
+            </Form.Item>
+          </div>
+          <div
+            style={{
+              display: "flex",
+              flexWrap: "wrap",
+              justifyContent: "space-between",
+            }}
           >
-            <Input
-              type="number"
-              value={numPeople.toString()}
-              onChange={(e) => setNumPeople(parseInt(e.target.value))}
-            />
-          </Form.Item>
-          <Form.Item
-            label="Fecha del Evento"
-            name="eventDate"
-            rules={[{ required: true, message: 'Este campo es obligatorio' }]}
-            style={{ width: '48%' }}
+            <Form.Item
+              label="CI del Cliente"
+              name="clientId"
+              rules={[{ required: true, message: "Este campo es obligatorio" }]}
+              style={{ width: "48%" }}
+            >
+              <Input
+                value={clientId}
+                onChange={(e) => setClientId(e.target.value)}
+              />
+            </Form.Item>
+            <Form.Item
+              label="Tipo de Servicio"
+              name="serviceType"
+              rules={[{ required: true, message: "Este campo es obligatorio" }]}
+              style={{ width: "48%" }}
+            >
+              <Select
+                value={serviceType}
+                onChange={handleServiceTypeChange}
+                style={{ width: "100%" }}
+              >
+                <Option value="option1">Opción 1</Option>
+                <Option value="option2">Opción 2</Option>
+                <Option value="option3">Opción 3</Option>
+                <Option value="option4">Opción 4</Option>
+              </Select>
+            </Form.Item>
+          </div>
+          <div
+            style={{
+              display: "flex",
+              flexWrap: "wrap",
+              justifyContent: "space-between",
+            }}
           >
-            <DatePicker
-              value={eventDate}
-              onChange={(date) => setEventDate(date)}
-              style={{ width: '100%' }}
-            />
-          </Form.Item>
-        </div>
-        <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'space-between' }}>
-          <Form.Item
-            label="Hora del Evento"
-            name="eventTime"
-            // rules={[{ required: true, message: 'Este campo es obligatorio' }]}
-            style={{ width: '48%' }}
+            <Form.Item
+              label="Número de Personas"
+              name="numPeople"
+              rules={[{ required: true, message: "Este campo es obligatorio" }]}
+              style={{ width: "48%" }}
+            >
+              <Input
+                type="number"
+                value={numPeople.toString()}
+                onChange={(e) => setNumPeople(parseInt(e.target.value))}
+              />
+            </Form.Item>
+            <Form.Item
+              label="Fecha del Evento"
+              name="eventDate"
+              rules={[{ required: true, message: "Este campo es obligatorio" }]}
+              style={{ width: "48%" }}
+            >
+              <DatePicker
+                value={eventDate}
+                onChange={(date) => setEventDate(date)}
+                style={{ width: "100%" }}
+              />
+            </Form.Item>
+          </div>
+          <div
+            style={{
+              display: "flex",
+              flexWrap: "wrap",
+              justifyContent: "space-between",
+            }}
           >
-            <Input
-              value={eventTime}
-              onChange={(e) => setEventTime(e.target.value)}
-            />
-          </Form.Item>
-          <Form.Item
-            label="Ubicación del Evento"
-            name="eventLocation"
-            rules={[{ required: true, message: 'Este campo es obligatorio' }]}
-            style={{ width: '48%' }}
+            <Form.Item
+              label="Hora del Evento"
+              name="eventTime"
+              // rules={[{ required: true, message: 'Este campo es obligatorio' }]}
+              style={{ width: "48%" }}
+            >
+              <Input
+                value={eventTime}
+                onChange={(e) => setEventTime(e.target.value)}
+              />
+            </Form.Item>
+            <Form.Item
+              label="Ubicación del Evento"
+              name="eventLocation"
+              rules={[{ required: true, message: "Este campo es obligatorio" }]}
+              style={{ width: "48%" }}
+            >
+              <Input
+                value={eventLocation}
+                onChange={(e) => setEventLocation(e.target.value)}
+              />
+            </Form.Item>
+          </div>
+          <div
+            style={{
+              display: "flex",
+              flexWrap: "wrap",
+              justifyContent: "space-between",
+            }}
           >
-            <Input
-              value={eventLocation}
-              onChange={(e) => setEventLocation(e.target.value)}
-            />
+            <Form.Item
+              label="Presupuesto"
+              name="presupuesto"
+              rules={[{ required: true, message: "Este campo es obligatorio" }]}
+              style={{ width: "48%" }}
+            >
+              <Input
+                type="number"
+                value={numPeople.toString()}
+                onChange={(e) => setpresupuesto(parseInt(e.target.value))}
+              />
+            </Form.Item>
+          </div>
+          <Form.Item>
+            <Button type="primary" htmlType="submit">
+              Agregar Evento
+            </Button>
           </Form.Item>
-        </div>
-        <Form.Item>
-          <Button type="primary" htmlType="submit">
-            Agregar Evento
-          </Button>
-        </Form.Item>
-      </Form>
-    </AddEventContainer>
-  </AddEventPageWrapper>
+        </Form>
+      </AddEventContainer>
+    </AddEventPageWrapper>
   );
 };
 
--- src/components/AgendaPage/AgendaPage.tsx
+++ src/components/AgendaPage/AgendaPage.tsx
@@ -1,18 +1,18 @@
-// AgendaPage.tsx
-import React, { useState, useEffect } from 'react';
-import { Calendar, Tooltip } from 'antd';
-import type { Dayjs } from 'dayjs';
-
-interface AgendaPageProps {}
+import React, { useState, useEffect } from "react";
+import { Calendar, Modal, Tooltip } from "antd";
+import { Dayjs } from "dayjs";
 
 interface Event {
   date: string;
   title: string;
   description: string;
 }
+interface AgendaPageProps {}
 
 const AgendaPage: React.FC<AgendaPageProps> = () => {
   const [events, setEvents] = useState<Event[]>([]);
+  const [selectedDate, setSelectedDate] = useState<Dayjs | null>(null);
+  const [isModalVisible, setIsModalVisible] = useState(false);
 
   useEffect(() => {
     // Obtener los eventos desde el backend o una fuente de datos
@@ -23,14 +23,14 @@ const AgendaPage: React.FC<AgendaPageProps> = () => {
     // Datos de ejemplo
     const exampleEvents: Event[] = [
       {
-        date: '2024-08-08',
-        title: 'Fiesta de 15',
-        description: 'Discutir el progreso del proyecto',
+        date: "2024-08-08",
+        title: "Fiesta de 15",
+        description: "Discutir el progreso del proyecto",
       },
       {
-        date: '2024-08-10',
-        title: 'Casamiento de Ramona',
-        description: 'Presentar el nuevo producto',
+        date: "2024-08-10",
+        title: "Casamiento de Ramona",
+        description: "Presentar el nuevo producto",
       },
       // Más eventos de ejemplo...
     ];
@@ -39,7 +39,7 @@ const AgendaPage: React.FC<AgendaPageProps> = () => {
   };
 
   const getListData = (value: Dayjs) => {
-    const date = value.format('YYYY-MM-DD');
+    const date = value.format("YYYY-MM-DD");
     const eventData = events.filter((event) => event.date === date);
     return eventData;
   };
@@ -57,10 +57,37 @@ const AgendaPage: React.FC<AgendaPageProps> = () => {
     );
   };
 
+  const onSelect = (value: Dayjs) => {
+    setSelectedDate(value);
+    setIsModalVisible(true);
+  };
+
+  const handleCancel = () => {
+    setIsModalVisible(false);
+  };
+
+  const getEventsBySelectedDate = () => {
+    if (!selectedDate) return [];
+    return getListData(selectedDate);
+  };
+
   return (
     <div className="agenda-page">
       <h1>Agenda</h1>
-      <Calendar dateCellRender={dateCellRender} />
+      <Calendar dateCellRender={dateCellRender} onSelect={onSelect} />
+      <Modal
+        title={selectedDate?.format("YYYY-MM-DD")}
+        visible={isModalVisible}
+        onCancel={handleCancel}
+        footer={null}
+      >
+        {getEventsBySelectedDate().map((event, index) => (
+          <div key={index}>
+            <h3>{event.title}</h3>
+            <p>{event.description}</p>
+          </div>
+        ))}
+      </Modal>
     </div>
   );
 };
